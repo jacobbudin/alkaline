a4e46b5e53bd844ca313c2f842ac684066dee6b4
Jacob Budin <me@jacobbudin.com>
2010-02-22 00:03:27 -0500

    Added .gitignore to ignore the crud

--- beta/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$html = '<!-- START(PHOTOS) --><p>alsdkja<p><!-- END(PHOTOS) -->';
-
-preg_match('(\<!--\s*START\(PHOTOS\)\s*-->(.*?)<!--\s*END\(PHOTOS\)\s*--\>)', $html, $matches);
-
-print_r($matches);
-
-?>
