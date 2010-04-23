123e47cb0af44f6554bb03fe2d2307e0f28faf76
Jacob Budin <me@jacobbudin.com>
2010-04-22 22:34:16 -0400

    Improved JS notification

--- beta/admin/tasks/add-notification.php
+++ beta/admin/tasks/add-notification.php
@@ -11,8 +11,8 @@ $user = new User;
 
 $user->perm(true);
 
-if(!empty($_POST['message'])){
-	$alkaline->addNotification($_POST['message'], $_POST['type']);
+if(@!empty($_POST['message'])){
+	@$alkaline->addNotification($_POST['message'], $_POST['type']);
 }
 
 ?>
