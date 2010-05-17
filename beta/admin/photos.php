c1065a2677cda8f340227b17359a36af0c6683e2
Jacob Budin <me@jacobbudin.com>
2010-05-17 15:34:39 -0400

    CP>Photos aesthetic mods; added grid, list icons

--- beta/admin/photos.php
+++ beta/admin/photos.php
@@ -29,18 +29,23 @@ require_once(PATH . ADMIN . 'includes/header.php');
 ?>
 
 <div id="content" class="span-<?php echo COLUMNS - 2; ?> prepend-1 append-1 last">
-	<form id="search" method="get" style="float: right; text-align: right; margin-top: 5px;">
-		<input type="text" name="find_text" style="width: 200px; font-size: .9em;" /> <input type="submit" value="Search" /><br />
-		<a href="" style="line-height: 2.5em;">Advanced search</a>
-	</form>
+	
+	<div id="view">
+		<a href="" class="selected"><img src="<?php echo BASE . IMAGES; ?>icons/grid.png" alt="" /> Grid view</a> <a href=""><img src="<?php echo BASE . IMAGES; ?>icons/list.png" alt="" /> List view</a>
+	</div>
 	
 	<h2>Photos</h2>
 	
-	<h3>Library</h3>
-	
 	<?php $alkaline->viewNotification(); ?>
 	
-	<p>Your library includes <?php echo $photo_ids->photo_count_result; ?> photos.</p>
+	<h3>Search</h3>
+
+	<form id="search" method="get">
+		<input type="text" name="find_text" style="width: 50%; font-size: .9em; margin-left: 0;" /> <input type="submit" value="Search" /><br />
+		<a href="" style="line-height: 2.5em;">Advanced search</a>
+	</form><br />
+	
+	<h3>Library <span class="small quiet">(<?php echo $photo_ids->photo_count_result; ?>)</span></h3>
 	
 	<div id="photos" class="span-17 last">
 		<?php
