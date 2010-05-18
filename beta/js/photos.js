3a0b59f0944f491114cae1708b89593049b66506
Jacob Budin <me@jacobbudin.com>
2010-05-18 15:47:56 -0400

    CP>Photos now loads data, ESC key in Grid view

--- beta/js/photos.js
+++ beta/js/photos.js
@@ -1,26 +1,42 @@
 var photo_id;
 
-$(document).ready(function(){
-	$("a.photo").click(function(){
-		$(".photo").children("img").removeClass("selected");
-		$(".blurb").slideUp();
-		if(photo_id != $(this).attr("id")){
-			photo_id = $(this).attr("id");
-			$(this).children("img").addClass("selected");
-			position = $(this).position();
+function photoSelect(photo){
+	$(".photo").children("img").removeClass("selected");
+	$(".blurb").slideUp();
+	if(typeof(photo) == 'object'){
+		if(photo_id != photo.attr("id")){
+			photo_id = photo.attr("id");
+			photo.children("img").addClass("selected");
+			position = photo.position();
 			position_top = String(position.top + 11);
 			background_position = String(-960 + position.left);
 			$("#" + photo_id + "-blurb").css("top", position_top + "px");
 			$("#" + photo_id + "-blurb").css("background-position", background_position + "px 0");					
 			$("#" + photo_id + "-blurb").slideDown();
-			window.setTimeout(scrollIt, 400);
+			window.setTimeout(scroll, 400);
 		}
 		else{
 			photo_id = null;
 		}
-		function scrollIt(){	
-			$(document).scrollTop(position_top+400);
-		}
+	}
+	else{
+		photo_id = null;
+	}
+}
+
+function scroll(){	
+	$(document).scrollTop(position_top+400);
+}
+
+$(document).ready(function(){
+	$("a.photo").click(function(){
+		photoSelect($(this));
 		event.preventDefault();
+	});
+	$(document).keydown(function(event){
+		if(event.keyCode == '27'){
+			photoSelect(0);
+			event.preventDefault();
+		}
 	});
 });
