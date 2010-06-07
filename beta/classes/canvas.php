975ce951a15d17670e4761e8c3e2fa6f0bed2b99
Jacob Budin <me@jacobbudin.com>
2010-06-07 16:20:33 -0400

    ::Canvas optimizations

--- beta/classes/canvas.php
+++ beta/classes/canvas.php
@@ -1,9 +1,7 @@
 <?php
 
 class Canvas extends Alkaline{
-	public $arrays;
 	public $template;
-	public $variables;
 	
 	public function __construct($template=null){
 		parent::__construct();
@@ -22,38 +20,51 @@ class Canvas extends Alkaline{
 		return $this->template;
 	}
 	
+	// APPEND TEMPLATE
 	public function append($template){
 		 $this->template .= $template . "\n";
 	}
 	
+	// LOAD TEMPLATE FROM FILE
 	public function load($file){
 		 $this->template .= file_get_contents(PATH . THEMES . THEME . '/' . $file . TEMP_EXT) . "\n";
 	}
 	
 	public function setVar($var, $value){
-		// Set variable; since used, remove conditionals
-		$this->variables->$var = $value;
-		$this->template = preg_replace('(\<!-- ' . $var . ' --\>)', $value, $this->template);
-		$this->template = preg_replace('(\<!-- IF\(' . $var . '\) --\>)', '', $this->template);
-		$this->template = preg_replace('(\<!-- ENDIF\(' . $var . '\) --\>)', '', $this->template);
+		// Set variable
+		$this->template = str_replace('<!-- ' . $var . ' -->', $value, $this->template);
+		
+		// Remove conditional
+		$this->template = str_replace('<!-- IF(' . $var . ') -->', '', $this->template);
+		$this->template = str_replace('<!-- ENDIF(' . $var . ') -->)', '', $this->template);
+		return true;
 	}
 	
-	public function setArray($array, $prefix, $value){
+	public function setArray($reel, $prefix, $array){
 		// Set array; since used, remove conditionals
-		$this->arrays->$array = $value;
-		$this->template = preg_replace('(\<!-- IF\(' . $array . '\) --\>)', '', $this->template);
-		$this->template = preg_replace('(\<!-- ENDIF\(' . $array . '\) --\>)', '', $this->template);
-		preg_match('/\<!-- LOOP\(' . $array . '\) --\>(.*)\<!-- ENDLOOP\(' . $array . '\) --\>/s', $this->template, $matches);
+		$this->template = str_replace('<!-- IF(' . $reel . ') -->', '', $this->template);
+		$this->template = str_replace('<!-- ENDIF(' . $reel . ') -->', '', $this->template);
+		preg_match('/\<!-- LOOP\(' . $reel . '\) --\>(.*)\<!-- ENDLOOP\(' . $reel . '\) --\>/s', $this->template, $matches);
 		@$loop_template = $matches[1];
 		$template = '';
-		foreach($this->arrays->$array as $units){
+		
+		// Loop through each set, append to empty string
+		for($i = 0; $i < count($array); ++$i){
 			$loop = $loop_template;
-			foreach($units as $key => $value){
-				$loop = @str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop);
+			foreach($array[$i] as $key => $value){
+				if(is_array($value)){
+					// DO SOMETHING HERE
+				}
+				else{
+					$loop = @str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop);
+				}
 			}
 			$template .= $loop;
 		}
-		$this->template = preg_replace('/\<!-- LOOP\(' . $array . '\) --\>(.*)\<!-- ENDLOOP\(' . $array . '\) --\>/s', $template, $this->template);
+		
+		// Replace loop template with string
+		$this->template = preg_replace('/\<!-- LOOP\(' . $reel . '\) --\>(.*)\<!-- ENDLOOP\(' . $reel . '\) --\>/s', $template, $this->template);
+		
 		return true;
 	}
 	
@@ -61,6 +72,7 @@ class Canvas extends Alkaline{
 		// Remove unused conditionals, replace with ELSEIF as available
 		$this->template = preg_replace('/\<!-- IF\([A-Z0-9_]*\) --\>(.*?)\<!-- ELSEIF\([A-Z0-9_]*\) --\>(.*?)\<!-- ENDIF\([A-Z0-9_]*\) --\>/s', '$2', $this->template);
 		$this->template = preg_replace('/\<!-- IF\([A-Z0-9_]*\) --\>(.*?)\<!-- ENDIF\([A-Z0-9_]*\) --\>/s', '', $this->template);
+		
 		return true;
 	}
 	
