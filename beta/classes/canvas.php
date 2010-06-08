959bc98e926e2bc94b95bf9b847eda8eecbeeace
Jacob Budin <me@jacobbudin.com>
2010-06-08 14:51:05 -0400

    ::Canvas works with $photos->comments

--- beta/classes/canvas.php
+++ beta/classes/canvas.php
@@ -41,24 +41,60 @@ class Canvas extends Alkaline{
 	}
 	
 	public function setArray($reel, $prefix, $array){
+		
 		// Set array; since used, remove conditionals
 		$this->template = str_replace('<!-- IF(' . $reel . ') -->', '', $this->template);
 		$this->template = str_replace('<!-- ENDIF(' . $reel . ') -->', '', $this->template);
 		preg_match('/\<!-- LOOP\(' . $reel . '\) --\>(.*)\<!-- ENDLOOP\(' . $reel . '\) --\>/s', $this->template, $matches);
 		@$loop_template = $matches[1];
+		
+		// Set array; since used, remove conditionals
+		$this->template = str_replace('<!-- IF(COMMENTS) -->', '', $this->template);
+		$this->template = str_replace('<!-- ENDIF(COMMENTS) -->', '', $this->template);
+		preg_match('/\<!-- LOOP\(COMMENTS\) --\>(.*)\<!-- ENDLOOP\(COMMENTS\) --\>/s', $this->template, $matches);
+		@$loop_commments_template = $matches[1];
+		
 		$template = '';
 		
 		// Loop through each set, append to empty string
-		for($i = 0; $i < count($array); ++$i){
+		for($i = 0; $i < count($array->photos); ++$i){
 			$loop = $loop_template;
-			foreach($array[$i] as $key => $value){
+			
+			foreach($array->photos[$i] as $key => $value){
 				if(is_array($value)){
-					// DO SOMETHING HERE
+					$value = var_export($value, true);
+					$loop = str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop);
 				}
 				else{
-					$loop = @str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop);
+					$loop = str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop);
 				}
 			}
+			
+			
+			if(!empty($loop_commments_template)){
+				$comments_template = '';
+			
+				for($j = 0; $j < count($array->comments); ++$j){
+					$loop_commments = $loop_commments_template;
+					
+					foreach($array->comments[$j] as $key => $value){
+						if($array->comments[$j]['photo_id'] == $array->photos[$i]['photo_id']){
+							if(is_array($value)){
+								$value = var_export($value, true);
+								$loop_commments = str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop_commments);
+							}
+							else{
+								$loop_commments = str_replace('<!-- ' . strtoupper($key) . ' -->', $value, $loop_commments);
+							}
+						}
+					}
+					$comments_template .= $loop_commments;
+				}
+			
+				// Replace comments loop template with string
+				$loop = preg_replace('/\<!-- LOOP\(COMMENTS\) --\>(.*)\<!-- ENDLOOP\(COMMENTS\) --\>/s', $comments_template, $loop);
+			}
+			
 			$template .= $loop;
 		}
 		
