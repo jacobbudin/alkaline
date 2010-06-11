328cfef8577eb82e9b3e3a3bb0be38c465ea7e8e
Jacob Budin <me@jacobbudin.com>
2010-06-11 16:37:17 -0400

    Twitter Sync, Orbit-powered, basics works

--- beta/extensions/twitter.php
+++ beta/extensions/twitter.php
@@ -4,9 +4,28 @@ class Twitter extends Orbit{
 	public $twitter;
 	
 	public function __construct(){
-		parent::__construct('Twitter');
-		$this->fetch('twitter.php');
-		$this->twitter = new Twitter_Helper('alkalineapp', 'gkek573');
+		parent::__construct('07a3f3d1b494c43417ff002ef659bebb687b75e4');
+		
+		$this->fetch('EpiCurl.php');
+		$this->fetch('EpiOAuth.php');
+		$this->fetch('EpiTwitter.php');
+		
+		$oauth_token = $this->readPref('oauth_token');
+		$oauth_secret = $this->readPref('oauth_secret');
+		$profile_image_url = $this->readPref('profile_image_url');
+		$screen_name = $this->readPref('screen_name');
+		
+		$this->twitter = new EpiTwitter('Ss0F1kxtvxkkmKGgvPx8w',
+			't55gKYkDtn5uKo1enMyF1E00RwOec9aDzNo7TFhzZx4',
+			$oauth_token,
+			$oauth_secret);
+		
+		if(empty($profile_image_url) or empty($screen_name)){
+			$user = $this->twitter->get('/account/verify_credentials.json');
+			$this->setPref('profile_image_url', $user->profile_image_url);
+			$this->setPref('screen_name', $user->screen_name);
+			$this->savePref();
+		}
 	}
 	
 	public function __destruct(){
@@ -15,7 +34,8 @@ class Twitter extends Orbit{
 	
 	function photo_upload($photo_ids){
 		$status = 'I just uploaded a photo.';
-		echo $this->twitter->updateStatus($status);
+		$paramaters = array('status' => $status);
+		// $this->twitter->post('/statuses/update.json', $paramaters);
 	}
 }
 
