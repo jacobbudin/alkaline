97ce99e11cfba19e98154f6e385ff784450ceb53
Jacob Budin <me@jacobbudin.com>
2010-06-14 10:54:17 -0400

    Orbit:: improvements

--- beta/classes/orbit.php
+++ beta/classes/orbit.php
@@ -1,34 +1,43 @@
 <?php
 
 class Orbit extends Alkaline{
-	public $class;
+	public $id;
 	public $uid;
-	public $extensions;
-	public $extension_count;
+	
+	public $class;
+	public $file;
+	public $hooks;
 	public $preferences;
-	public $sandbox;
+	public $title;
+	
+	public $extensions;
+	
+	public $extension_count;
+	
 	private $db_safe;
 	
-	public function __construct($uid=null){
+	public function __construct($identifier=null){
 		parent::__construct();
 		
-		$this->uid = $uid;
 		$this->db_safe = $this->db;
 		unset($this->db);
 		
-		if(empty($uid)){
-			$query = $this->db_safe->prepare('SELECT extension_uid, extension_title, extension_class, extension_hooks, extension_preferences FROM extensions WHERE extension_status > 0 ORDER BY extension_build ASC, extension_id ASC;');
+		if(empty($identifier)){
+			$query = $this->db_safe->prepare('SELECT extension_id, extension_uid, extension_title, extension_class, extension_hooks, extension_preferences FROM extensions WHERE extension_status > 0 ORDER BY extension_build ASC, extension_id ASC;');
 			$query->execute();
 			$extensions = $query->fetchAll();
 
 			$this->extensions = array();
 
 			foreach($extensions as $extension){
+				$extension_id = $extension['extension_id'];
 				$extension_uid = strval($extension['extension_uid']);
 				$extension_file = PATH . EXTENSIONS . strtolower($extension['extension_class']) . '.php';
 				$extension_title = $extension['extension_title'];
 				$extension_hooks = unserialize($extension['extension_hooks']);
-				$this->extensions[$extension_uid] = array('extension_file' => $extension_file,
+				$this->extensions[] = array('extension_id' => $extension_id,
+					'extension_uid' => $extension_uid,
+					'extension_file' => $extension_file,
 					'extension_title' => $extension_title,
 					'extension_class' => $extension['extension_class'],
 					'extension_hooks' => $extension_hooks);
@@ -37,7 +46,14 @@ class Orbit extends Alkaline{
 			$this->extension_count = count($this->extensions);
 		}
 		else{
-			$query = $this->db_safe->prepare('SELECT extension_class, extension_preferences FROM extensions WHERE extension_uid = "' . $this->uid . '" AND extension_status > 0;');
+			if(strlen($identifier) == 40){
+				$this->uid = $identifier;
+				$query = $this->db_safe->prepare('SELECT * FROM extensions WHERE extension_uid = "' . $this->uid . '" AND extension_status > 0;');
+			}
+			else{
+				$this->id = $identifier;
+				$query = $this->db_safe->prepare('SELECT * FROM extensions WHERE extension_id = "' . $this->id . '" AND extension_status > 0;');
+			}
 			$query->execute();
 			$extensions = $query->fetchAll();
 			
@@ -45,7 +61,26 @@ class Orbit extends Alkaline{
 				return false;
 			}
 			
+			foreach($extensions as $extension){
+				$extension_id = $extension['extension_id'];
+				$extension_uid = strval($extension['extension_uid']);
+				$extension_file = PATH . EXTENSIONS . strtolower($extension['extension_class']) . '.php';
+				$extension_title = $extension['extension_title'];
+				$extension_hooks = unserialize($extension['extension_hooks']);
+				$this->extensions[] = array('extension_id' => $extension_id,
+					'extension_uid' => $extension_uid,
+					'extension_file' => $extension_file,
+					'extension_title' => $extension_title,
+					'extension_class' => $extension['extension_class'],
+					'extension_hooks' => $extension_hooks);
+			}
+			
+			$this->id = $extensions[0]['extension_id'];
+			$this->uid = strval($extensions[0]['extension_uid']);
+			$this->title = $extensions[0]['extension_title'];
+			$this->file = PATH . EXTENSIONS . strtolower($extensions[0]['extension_class']) . '.php';
 			$this->class = $extensions[0]['extension_class'];
+			$this->hooks = unserialize($extensions[0]['extension_hooks']);
 			$this->preferences = unserialize($extensions[0]['extension_preferences']);
 		}
 		return true;
@@ -91,11 +126,13 @@ class Orbit extends Alkaline{
 		$arguments = array_slice($arguments, 1);
 		
 		// Find respective extensions, execute their code
-		foreach($this->extensions as $extension){
-			if(in_array($hook, $extension['extension_hooks'])){
-				include($extension['extension_file']);
-				$orbit = new $extension['extension_class']();
-				call_user_func_array(array($orbit, $hook), $arguments);
+		if(!empty($this->extensions)){
+			foreach($this->extensions as $extension){
+				if(in_array($hook, $extension['extension_hooks'])){
+					include($extension['extension_file']);
+					$orbit = new $extension['extension_class']();
+					call_user_func_array(array($orbit, $hook), $arguments);
+				}
 			}
 		}
 	}
