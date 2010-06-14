23fe87dc95b38cbda8a1afc2a6363c2a2609acfe
Jacob Budin <me@jacobbudin.com>
2010-06-14 10:54:33 -0400

    Added Extension config to dash

--- beta/admin/extensions.php
+++ beta/admin/extensions.php
@@ -6,10 +6,13 @@ require_once(PATH . CLASSES . 'orbit.php');
 require_once(PATH . CLASSES . 'user.php');
 
 $alkaline = new Alkaline;
-$orbit = new Orbit;
 $user = new User;
 
 $user->perm(true);
+
+$extension_id = @$alkaline->findID($_GET['identifier']);
+
+$orbit = new Orbit($extension_id);
 
 define('TITLE', 'Alkaline Extensions');
 define('COLUMNS', '19');
@@ -21,23 +24,31 @@ require_once(PATH . ADMIN . 'includes/header.php');
 
 <div id="content" class="span-<?php echo COLUMNS - 2; ?> prepend-1 append-1 last">
 	<h2>Extensions</h2>
-	
-	<?php $alkaline->viewNotification(); ?>
-	
-	<h3>Available <span class="small quiet">(<span id="count"><?php echo @$orbit->extension_count; ?></span>)</span></h3>
-	
-	<hr />
-	
-	<?php
-	
-	foreach($orbit->extensions as $extension){
-		echo '<p>' . $extension['extension_title'] . '</p>';
-	}
-	
-	?>
-	
-</div>
 
+	<?php $alkaline->viewNotification(); ?>
+<?php
+if(!empty($extension_id)){
+	?>
+	<h3><?php echo $orbit->title; ?></h3>
+
+	<hr />
+
+	<?php
+	$orbit->hook('config');
+}
+else{	
+	?>
+	<h3>Available <span class="small quiet">(<span id="count"><?php echo @$orbit->extension_count; ?></span>)</span></h3>
+
+	<hr />
+
+	<?php
+	foreach($orbit->extensions as $extension){
+		echo '<p>' . $extension['extension_title'] . ' (<a href="?identifier=' . $extension['extension_id'] . '-' . $extension['extension_class']  . '">Config</a>)</p>';
+	}
+}
+?>
+</div>
 <?php
 
 require_once(PATH . ADMIN . 'includes/footer.php');
