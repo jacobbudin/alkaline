df8588cc5bb0d3153810badb5b662097e9e8a483
Jacob Budin <me@jacobbudin.com>
2010-06-24 16:13:31 -0400

    Added photo page

---
+++ beta/photo.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('./config.php');
+require_once(PATH . CLASSES . 'alkaline.php');
+
+$alkaline = new Alkaline;
+$alkaline->recordStat('photo');
+
+$id = $alkaline->findID($_GET['identifier']);
+
+$orbit = new Orbit;
+
+$photos = new Photo($id);
+$photos->updateViews();
+$photos->formatTime();
+$photos->getImgUrl('medium');
+$photos->getExif();
+$photos->getTags();
+$photos->getRights();
+$photos->getComments();
+
+$header = new Canvas;
+$header->load('header');
+$header->assign('TITLE', $photos->photos[0]['photo_title']);
+$header->display();
+
+
+$index = new Canvas;
+$index->load('photo');
+$index->loop($photos);
+$index->display();
+
+$footer = new Canvas;
+$footer->load('footer');
+$footer->display();
+
+?>
