099d0a97116f53de583af4be952f061ce482b815
Jacob Budin <jbudin@budinltd.com>
2010-08-17 14:36:56 -0400

    Added working Users panel to Dash

---
+++ beta/admin/users.php
@@ -0,0 +1,130 @@
+<?php
+
+require_once('./../config.php');
+require_once(PATH . CLASSES . 'alkaline.php');
+
+$alkaline = new Alkaline;
+$user = new User;
+
+$user->perm(true);
+
+$user_id = @$alkaline->findID($_GET['id']);
+
+// SAVE CHANGES
+if(!empty($_POST['user_id'])){
+	$user_id = $alkaline->findID($_POST['user_id']);
+	if(@$_POST['user_delete'] == 'delete'){
+		$alkaline->deleteRow('users', $user_id);
+	}
+	else{
+		$fields = array('user_name' => $_POST['user_name'],
+			'user_user' => $_POST['user_user'],
+			'user_email' => $_POST['user_email']);
+		$alkaline->updateRow($fields, 'users', $user_id);
+	}
+	unset($user_id);
+}
+
+// GET USERS TO VIEW OR USER TO EDIT
+if(empty($user_id)){
+
+	$users = $alkaline->getTable('users');
+	$user_count = @count($users);
+	
+	define('TITLE', 'Alkaline Users');
+	require_once(PATH . ADMIN . 'includes/header.php');
+	require_once(PATH . ADMIN . 'includes/settings.php');
+
+	?>
+
+	<h1>Users (<?php echo $user_count; ?>)</h1>
+	
+	<table>
+		<tr>
+			<th>Name</th>
+			<th class="center">Photos</th>
+			<th>Last login</th>
+		</tr>
+		<?php
+	
+		foreach($users as $user){
+			echo '<tr>';
+				echo '<td><strong><a href="' . BASE . ADMIN . 'users/' . $user['user_id'] . '">' . $user['user_user'] . '</a></strong> <em>' . $user['user_name'] . '</em></td>';
+				echo '<td class="center">~' . $user['user_photo_count'] . '</td>';
+				echo '<td>' . $alkaline->formatTime($user['user_last_login']) . '</td>';
+			echo '</tr>';
+		}
+	
+		?>
+	</table>
+	
+	<?php
+	
+	require_once(PATH . ADMIN . 'includes/footer.php');
+	
+}
+else{
+	
+	// Update photo count on user
+	$photo_ids = new Find;
+	$photo_ids->user($user_id);
+	$photo_ids->exec();
+	
+	$fields = array('user_photo_count' => $photo_ids->photo_count);
+	$alkaline->updateRow($fields, 'users', $user_id, false);
+	
+	// Get user
+	$users = $alkaline->getTable('users', $user_id);
+	$user = $users[0];
+
+	if(!empty($user['user_title'])){	
+		define('TITLE', 'Alkaline user: &#8220;' . $user['user_title']  . '&#8221;');
+	}
+	else{
+		define('TITLE', 'Alkaline user');
+	}
+	require_once(PATH . ADMIN . 'includes/header.php');
+	require_once(PATH . ADMIN . 'includes/settings.php');
+
+	?>
+	
+	<div style="float: right; margin: 1em 0;"><a href="<?php echo BASE . ADMIN; ?>search/users/<?php echo $user['user_id']; ?>/" class="nu"><span class="button">&#0187;</span>View photos</a></div>
+	
+	<h1>User</h1>
+	
+	<form id="user" action="<?php echo BASE . ADMIN; ?>users/" method="post">
+		<table>
+			<tr>
+				<td class="right middle"><label for="user_name">Name:</label></td>
+				<td><input type="text" id="user_name" name="user_name" value="<?php echo $user['user_name']; ?>" class="title" /></td>
+			</tr>
+			<tr>
+				<td class="right middle"><label for="user_user">Username:</label></td>
+				<td><input type="text" id="user_user" name="user_user" value="<?php echo $user['user_user']; ?>" /></td>
+			</tr>
+			<tr>
+				<td class="right middle"><label for="user_email">Email:</label></td>
+				<td><input type="text" id="user_email" name="user_email" value="<?php echo $user['user_email']; ?>" style="width: 24em;" /></td>
+			</tr>
+			<tr>
+				<td class="right"><input type="checkbox" id="user_reset_pass" name="user_reset_pass" value="reset_pass" /></td>
+				<td><strong><label for="user_reset_pass">Reset password.</label></strong> This action cannot be undone.</td>
+			</tr>
+			<tr>
+				<td class="right"><input type="checkbox" id="user_delete" name="user_delete" value="delete" /></td>
+				<td><strong><label for="user_delete">Delete this user.</label></strong> This action cannot be undone.</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>" /><input type="submit" value="Save changes" /> or <a href="<?php echo BASE . ADMIN; ?>users/">cancel</a></td>
+			</tr>
+		</table>
+	</form>
+
+	<?php
+	
+	require_once(PATH . ADMIN . 'includes/footer.php');
+	
+}
+
+?>
