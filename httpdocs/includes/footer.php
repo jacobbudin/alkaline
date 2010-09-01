34002dda84d4a9aeb951f5bd058ddfd2a1e5ea3f
Jacob Budin <jbudin@budinltd.com>
2010-09-01 15:01:15 -0400

    Added FluxBB for users section

---
+++ httpdocs/includes/footer.php
@@ -0,0 +1,43 @@
+		<div id="sitemap" class="span-21 prepend-1 append-1 last">
+			<ul>
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/overview/">Overview</a></li>
+					<li><a href="/showcase/">Showcase</a></li>
+				</ul>
+				<ul>
+					<li><a href="/features/">Features</a></li>
+					<li><a href="/switch/">Migration</a></li>
+					<li><a href="/features/themes/">Themes</a></li>
+					<li><a href="/features/extensions/">Extensions</a></li>
+				</ul>
+				<ul>
+					<li><a href="/guide/">User Guide</a></li>
+					<li><a href="/guide/api/">API Documentation</a></li>
+					<li><a href="/guide/screencasts/">Screencasts</a></li>
+				</ul>
+				<ul>
+					<li><a href="/support/">Support</a></li>
+					<li><a href="/support/knowledge/">Knowledge Base</a></li>
+				</ul>
+				<ul>
+					<li><a href="/users/">Lounge</a></li>
+					<li><a href="/users/community/">Community</a></li>
+					<li><a href="/users/themes/">Themes</a></li>
+					<li><a href="/users/extensions/">Extensions</a></li>
+				</ul>
+				<ul>
+					<li><a href="/buy/">Buy Now</a></li>
+					<li><a href="/services/">Services</a></li>
+					<li><a href="/users/download/">Download</a></li>
+					<li><a href="/users/licenses/">Licenses</a></li>
+				</ul>
+			</ul>
+		</div>
+		<hr />
+		<div id="footer" class="span-21 prepend-1 append-1 last">
+			Copyright &#0169; 2010 by <a href="http://www.budinltd.com/">Budin Ltd.</a> All rights reserved.
+		</div>
+	</div>
+</body>
+</html>
