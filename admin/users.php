3f44c4ed050dba6e82af1578a905bc7d5e29f497
Jacob Budin <jbudin@budinltd.com>
2010-10-04 16:01:41 -0400

    Dash>Users bug fix

--- admin/users.php
+++ admin/users.php
@@ -8,14 +8,14 @@ $user = new User;
 
 $user->perm(true);
 
-$user_id = @$alkaline->findID($_GET['id']);
-$user_add = @$alkaline->findID($_GET['add']);
+$user_db_id = @$alkaline->findID($_GET['id']);
+$user_db_add = @$alkaline->findID($_GET['add']);
 
 // SAVE CHANGES
 if(!empty($_POST['user_id'])){
-	$user_id = $alkaline->findID($_POST['user_id']);
+	$user_db_id = $alkaline->findID($_POST['user_id']);
 	if(@$_POST['user_delete'] == 'delete'){
-		$alkaline->deleteRow('users', $user_id);
+		$alkaline->deleteRow('users', $user_db_id);
 	}
 	else{
 		$fields = array('user_name' => $alkaline->makeUnicode($_POST['user_name']),
@@ -24,26 +24,26 @@ if(!empty($_POST['user_id'])){
 		if(!empty($_POST['user_pass'])){
 			$fields['user_pass'] = sha1($_POST['user_pass']);
 		}
-		$alkaline->updateRow($fields, 'users', $user_id);
+		$alkaline->updateRow($fields, 'users', $user_db_id);
 	}
-	unset($user_id);
+	unset($user_db_id);
 }
 else{
 	$alkaline->deleteEmptyRow('users', array('user_name'));
 }
 
 // CREATE User
-if($user_add == 1){
-	$user_id = $alkaline->addRow(null, 'users');
+if($user_db_add == 1){
+	$user_db_id = $alkaline->addRow(null, 'users');
 }
 
 define('TAB', 'settings');
 
 // GET USERS TO VIEW OR USER TO EDIT
-if(empty($user_id)){
+if(empty($user_db_id)){
 
-	$users = $alkaline->getTable('users');
-	$user_count = @count($users);
+	$user_dbs = $alkaline->getTable('users');
+	$user_db_count = @count($user_dbs);
 	
 	define('TITLE', 'Alkaline Users');
 	require_once(PATH . ADMIN . 'includes/header.php');
@@ -52,7 +52,7 @@ if(empty($user_id)){
 	
 	<div class="actions"><a href="add/">Add user</a></div>
 
-	<h1>Users (<?php echo $user_count; ?>)</h1>
+	<h1>Users (<?php echo $user_db_count; ?>)</h1>
 	
 	<table>
 		<tr>
@@ -63,12 +63,12 @@ if(empty($user_id)){
 		</tr>
 		<?php
 	
-		foreach($users as $user){
+		foreach($user_dbs as $user_db){
 			echo '<tr>';
-				echo '<td><strong><a href="' . BASE . ADMIN . 'users/' . $user['user_id'] . '">' . $user['user_user'] . '</a></strong></td>';
-				echo '<td>' . $user['user_name'] . '</td>';
-				echo '<td class="center">' . number_format($user['user_photo_count']) . '</td>';
-				echo '<td>' . $alkaline->formatTime($user['user_last_login'], null, '<em>(Never)</em>') . '</td>';
+				echo '<td><strong><a href="' . BASE . ADMIN . 'users/' . $user_db['user_id'] . '">' . $user_db['user_user'] . '</a></strong></td>';
+				echo '<td>' . $user_db['user_name'] . '</td>';
+				echo '<td class="center">' . number_format($user_db['user_photo_count']) . '</td>';
+				echo '<td>' . $alkaline->formatTime($user_db['user_last_login'], null, '<em>(Never)</em>') . '</td>';
 			echo '</tr>';
 		}
 	
@@ -84,19 +84,19 @@ else{
 	
 	// Update photo count on user
 	$photo_ids = new Find;
-	$photo_ids->user($user_id);
+	$photo_ids->user($user_db_id);
 	$photo_ids->find();
 	
 	$fields = array('user_photo_count' => $photo_ids->photo_count);
-	$alkaline->updateRow($fields, 'users', $user_id, false);
+	$alkaline->updateRow($fields, 'users', $user_db_id, false);
 	
 	// Get user
-	$users = $alkaline->getTable('users', $user_id);
-	$user = $users[0];
-	$user = $alkaline->makeHTMLSafe($user);
+	$user_dbs = $alkaline->getTable('users', $user_db_id);
+	$user_db = $user_dbs[0];
+	$user_db = $alkaline->makeHTMLSafe($user_db);
 
-	if(!empty($user['user_name'])){
-		define('TITLE', 'Alkaline User: ' . $user['user_name']);
+	if(!empty($user_db['user_name'])){
+		define('TITLE', 'Alkaline User: ' . $user_db['user_name']);
 	}
 	else{
 		define('TITLE', 'Alkaline user');
@@ -105,7 +105,7 @@ else{
 
 	?>
 	
-	<div class="actions"><a href="<?php echo BASE . ADMIN; ?>search/users/<?php echo $user['user_id']; ?>/">View photos</a></div>
+	<div class="actions"><a href="<?php echo BASE . ADMIN; ?>search/users/<?php echo $user_db['user_id']; ?>/">View photos</a></div>
 	
 	<h1>User</h1>
 	
@@ -113,11 +113,11 @@ else{
 		<table>
 			<tr>
 				<td class="right middle"><label for="user_name">Name:</label></td>
-				<td><input type="text" id="user_name" name="user_name" value="<?php echo $user['user_name']; ?>" class="title" /></td>
+				<td><input type="text" id="user_name" name="user_name" value="<?php echo $user_db['user_name']; ?>" class="title" /></td>
 			</tr>
 			<tr>
 				<td class="right middle"><label for="user_user">Username:</label></td>
-				<td><input type="text" id="user_user" name="user_user" value="<?php echo $user['user_user']; ?>" /></td>
+				<td><input type="text" id="user_user" name="user_user" value="<?php echo $user_db['user_user']; ?>" /></td>
 			</tr>
 			<tr>
 				<td class="right pad"><label for="user_pass">New password:</label></td>
@@ -125,7 +125,7 @@ else{
 			</tr>
 			<tr>
 				<td class="right middle"><label for="user_email">Email:</label></td>
-				<td><input type="text" id="user_email" name="user_email" value="<?php echo $user['user_email']; ?>" style="width: 24em;" /></td>
+				<td><input type="text" id="user_email" name="user_email" value="<?php echo $user_db['user_email']; ?>" style="width: 24em;" /></td>
 			</tr>
 			<tr>
 				<td class="right"><input type="checkbox" id="user_reset_pass" name="user_reset_pass" value="reset_pass" /></td>
@@ -137,7 +137,7 @@ else{
 			</tr>
 			<tr>
 				<td></td>
-				<td><input type="hidden" name="user_id" value="<?php echo $user['user_id']; ?>" /><input type="submit" value="Save changes" /> or <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">cancel</a></td>
+				<td><input type="hidden" name="user_id" value="<?php echo $user_db['user_id']; ?>" /><input type="submit" value="Save changes" /> or <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">cancel</a></td>
 			</tr>
 		</table>
 	</form>
