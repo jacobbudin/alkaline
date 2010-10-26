3486464a8f0ca5b4d8752b5dda08df219741ad9d
Jacob Budin <me@jacobbudin.com>
2010-10-25 21:31:12 -0400

    Fixed URL rewriting on Dash>Pages

--- admin/pages.php
+++ admin/pages.php
@@ -90,7 +90,7 @@ if(empty($page_id)){
 
 			foreach($pages->pages as $page){
 				echo '<tr>';
-					echo '<td><a href="' . BASE . ADMIN . 'pages' . URL_ID . $page['page_id'] . URL_RW . '"><strong>' . $page['page_title'] . '</strong></a><br /><a href="' . BASE . 'pages/' . $page['page_title_url'] . '" class="nu">/' . $page['page_title_url'] . '</td>';
+					echo '<td><a href="' . BASE . ADMIN . 'pages' . URL_ID . $page['page_id'] . URL_RW . '"><strong>' . $page['page_title'] . '</strong></a><br /><a href="' . BASE . 'pages' . URL_ID . $page['page_title_url'] . URL_RW . '" class="nu">/' . $page['page_title_url'] . '</td>';
 					echo '<td class="center">' . number_format($page['page_views']) . '</td>';
 					echo '<td class="center">' . number_format($page['page_words']) . '</td>';
 					echo '<td>' . $pages->formatTime($page['page_created']) . '</td>';
