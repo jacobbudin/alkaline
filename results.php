21dc017904bc79cd7a8eb8bbb6fc5f8ea21e486d
Jacob Budin <me@jacobbudin.com>
2011-02-17 15:16:38 -0500

    Search defaults to "image" type

--- results.php
+++ results.php
@@ -13,26 +13,7 @@ require_once(PATH . CLASSES . 'alkaline.php');
 $alkaline = new Alkaline;
 $alkaline->recordStat('home');
 
-if($_REQUEST['type'] == 'images'){
-	$image_ids = new Find;
-	$image_ids->sort('images.image_published', 'DESC');
-	$image_ids->published();
-	$image_ids->page();
-	$image_ids->privacy('public');
-	$image_ids->find();
-
-	$images = new Image($image_ids);
-	$images->formatTime();
-	$images->getSizes();
-	
-	$count = $image_ids->image_count;
-	$model = $image_ids;
-	$loop = $images;
-	
-	$content = new Canvas;
-	$content->load('results-images');
-}
-elseif($_REQUEST['type'] == 'posts'){
+if($_REQUEST['type'] == 'posts'){
 	$posts = new Post;
 	$posts->page(null, 3);
 	$posts->published();
@@ -53,13 +34,32 @@ elseif($_REQUEST['type'] == 'posts'){
 	$content = new Canvas;
 	$content->load('results-posts');
 }
+else{
+	$image_ids = new Find;
+	$image_ids->sort('images.image_published', 'DESC');
+	$image_ids->published();
+	$image_ids->page();
+	$image_ids->privacy('public');
+	$image_ids->find();
+
+	$images = new Image($image_ids);
+	$images->formatTime();
+	$images->getSizes();
+	
+	$count = $image_ids->image_count;
+	$model = $image_ids;
+	$loop = $images;
+	
+	$content = new Canvas;
+	$content->load('results-images');
+}
 
 $header = new Canvas;
 $header->load('header');
 $header->setTitle('Search Results (' . $count . ')');
 $header->display();
 
-$content->assign('Results_Count', $count);
+$content->assign('Results_Count', $count, true);
 $content->assign('Page_Next', $model->page_next);
 $content->assign('Page_Previous', $model->page_previous);
 $content->assign('Page_Next_URI', $model->page_next_uri);
