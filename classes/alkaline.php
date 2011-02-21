f5cc21b3c823ea2b58a8a823204a813132522188
Jacob Budin <me@jacobbudin.com>
2011-02-20 21:37:28 -0500

    Added Alkaline::getType()

--- classes/alkaline.php
+++ classes/alkaline.php
@@ -1202,6 +1202,25 @@ class Alkaline{
 		return $num;
 	}
 	
+	/**
+	 * Returns the type of the variable as a comparable string
+	 *
+	 * @param mixed $var Variable
+	 * @return string Type of variable
+	 */
+	function getType($var){
+		if(is_array($var)){ return 'array'; }
+		if(is_bool($var)){ return 'boolean'; }
+		if(is_float($var)){ return 'float'; }
+		if(is_int($var)){ return 'integer'; }
+		if(is_null($var)){ return 'NULL'; }
+		if(is_numeric($var)){ return 'numeric'; }
+		if(is_object($var)){ return 'object'; }
+		if(is_resource($var)){ return 'resource'; }
+		if(is_string($var)){ return 'string'; }
+		return 'unknown';
+	}
+	
 	// COMMENTS
 	
 	/**
