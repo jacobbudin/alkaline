4b2a592ad0f1d287886ce174d3a3994f55bf43c7
Jacob Budin <me@jacobbudin.com>
2011-02-25 17:07:59 -0500

    Fixed mismatched HTML tag

--- admin/thumbnails.php
+++ admin/thumbnails.php
@@ -123,7 +123,7 @@ if(empty($size_id)){
 			echo '<tr>';
 				echo '<td><strong><a href="' . BASE . ADMIN . 'thumbnails' . URL_ID . $size['size_id'] . URL_RW . '">' . $size['size_title'] . '</a></strong></td>';
 				echo '<td class="center">' . $size['size_width'] . ' &#0215; ' . $size['size_height'] . '</td>';
-				echo '<td class="center">' . ucwords($size['size_type']) . '</a></td>';
+				echo '<td class="center">' . ucwords($size['size_type']) . '</td>';
 				echo '<td class="center">{Image_Src_' . ucwords($size['size_label']) . '}</td>';
 			echo '</tr>';
 		}
