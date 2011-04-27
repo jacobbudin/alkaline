1f2149f50cce7093fdebc63aa9b9c59639ce55b6
Jacob Budin <me@jacobbudin.com>
2011-04-27 16:42:51 -0400

    Adds timeline view to Dash>Dash

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -15,6 +15,8 @@ $user = new User;
 
 $user->perm(true);
 
+$alkaline->setCallback();
+
 // Vitals
 $stats = new Stat(strtotime('-30 days'));
 $stats->getDaily();
@@ -83,38 +85,132 @@ require_once(PATH . ADMIN . 'includes/header.php');
 <hr />
 
 <div class="span-24 last">
-	<?php
+	<div class="actions">
+		<a href="<?php echo BASE . ADMIN . 'atom' . URL_CAP; ?>"><button>Subscribe</button></a>
+	</div>
 	
-	// Preference: recent_images
-	if($user->returnPref('recent_images') === true){
-		?>
-		<h1><img src="<?php echo BASE . ADMIN; ?>images/icons/timeline.png" alt="" /> Timeline</h1>
-		<p>
-			<?php
-		
-			// Preference: recent_images_limit
-			if(!$max = $user->returnPref('recent_images_limit')){
-				$max = 10;
-			}
-		
-			$image_ids = new Find('images');
-			$image_ids->page(1,$max);
-			$image_ids->sort('images.image_uploaded', 'DESC');
-			$image_ids->find();
-			$images = new Image($image_ids);
-			$images->getSizes('square');
+	<h1><img src="<?php echo BASE . ADMIN; ?>images/icons/timeline.png" alt="" /> Timeline</h1><br />
+	
 
-			foreach($images->images as $image){
-				?>
-				<a href="<?php echo BASE . ADMIN . 'image' . URL_ID . $image['image_id'] . URL_RW; ?>" class="nu">
-					<img src="<?php echo $image['image_src_square']; ?>" alt="" title="<?php echo $image['image_title']; ?>" class="frame tip" />
-				</a>
-				<?php
-			}
-			?>
-		</p>
 		<?php
+
+	$timestamps = array();
+	$items = array();
+	$types = array();
+
+	$comment_ids = new Find('comments');
+	$comment_ids->sort('comments.comment_created', 'DESC');
+	$comment_ids->page(1, 60);
+	$comment_ids->find();
+
+	$comments = new Comment($comment_ids);
+
+	for($i=0; $i < $comments->comment_count; $i++){
+		$timestamps[] = strtotime($comments->comments[$i]['comment_created']);
+		$items[] = $comments->comments[$i];
+		$types[] = 'comment';
 	}
+
+	$image_ids = new Find('images');
+	$image_ids->sort('images.image_modified', 'DESC');
+	$image_ids->page(1, 60);
+	$image_ids->find();
+
+	$images = new Image($image_ids);
+	$images->getSizes('square');
+
+	for($i=0; $i < $images->image_count; $i++){
+		$timestamps[] = strtotime($images->images[$i]['image_modified']);
+		$items[] = $images->images[$i];
+		$types[] = 'image';
+	}
+
+	$post_ids = new Find('posts');
+	$post_ids->sort('posts.post_modified', 'DESC');
+	$post_ids->page(1, 60);
+	$post_ids->find();
+
+	$posts = new Post($post_ids);
+
+	for($i=0; $i < $posts->post_count; $i++){
+		$timestamps[] = strtotime($posts->posts[$i]['post_modified']);
+		$items[] = $posts->posts[$i];
+		$types[] = 'post';
+	}
+
+	array_multisort($timestamps, SORT_DESC, $items, $types);
+
+	if(count($items) == 0){
+		echo 'Welcome to Alkaline, starting enjoying your new Web site to populate the timeline.';
+	}
+	else{
+		$timeline = array();
+		$modified_last = '';
+
+		for($i=0; $i < 60; $i++){
+			if(!isset($types[$i])){ continue; }
+	
+			$type = $types[$i];
+	
+			$modified = $alkaline->formatRelTime($timestamps[$i]);
+	
+			if($modified != $modified_last){
+				$timeline[$modified] = array();
+				$modified_last = $modified;
+			}
+	
+			ob_start();
+	
+			if($type == 'comment'){
+				echo '<p><strong><a href="' . BASE . ADMIN . 'comments' . URL_ID . $items[$i]['comment_id'] . URL_RW . '" class="large tip" title="' . $alkaline->fitStringByWord(strip_tags($items[$i]['comment_text']), 150) . '">';
+				echo $alkaline->fitStringByWord(strip_tags($items[$i]['comment_text']), 50);
+				echo '</a></strong><br />';
+				if(!empty($items[$i]['comment_author_name'])){
+					echo '<span class="quiet"><a href="">' . $items[$i]['comment_author_name'] . '</a>';
+				}
+				else{
+					'<em>Anonymous</em>';
+				}
+
+				if(!empty($items[$i]['comment_author_ip']) and empty($items[$i]['user_id'])){
+					echo ' (<a href="">' . $items[$i]['comment_author_ip'] . '</a>)</span>';
+				}
+				elseif(!empty($items[$i]['user_id'])){
+					echo ' (User)';
+				}
+				echo '</p>';
+		
+				$timeline[$modified][] = ob_get_contents();
+			}
+			elseif($type == 'image'){
+				echo '<a href="' . BASE . ADMIN . 'image' . URL_ID . $items[$i]['image_id'] . URL_RW . '" class="nu">
+					<img src="' . $items[$i]['image_src_square'] . '" alt="" title="' . $items[$i]['image_title'] . '" class="frame tip" />
+				</a>';
+		
+				$timeline[$modified][] = ob_get_contents();
+			}
+			elseif($type == 'post'){
+				echo '<p><strong class="large"><a href="' . BASE . ADMIN . 'posts' . URL_ID . $items[$i]['post_id'] . URL_RW . '" title="' . $alkaline->fitStringByWord(strip_tags($items[$i]['post_text']), 150) . '" class="tip">' . $items[$i]['post_title'] . '</a></strong></p>';
+		
+				$timeline[$modified][] = ob_get_contents();
+			}
+	
+			ob_end_clean();
+		}
+	}
+
+	echo '<table>';
+
+	foreach($timeline as $modified => $items){
+		echo '<tr><td class="right" style="width:15%;"><strong class="quiet">' . ucfirst($modified) . '</strong></td><td>' . "\n";
+		foreach($items as $item){
+			echo $item . "\n";
+		}
+		echo '</td></tr>' . "\n";
+	}
+
+	echo '</table>';
+
 	?>
 </div>
 
