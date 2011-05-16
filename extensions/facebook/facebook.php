35859221b585f865dcbbc85f11139a4ac3d46ef1
Jacob Budin <me@jacobbudin.com>
2011-05-16 17:52:00 -0400

    Orbit>Facebook now works

--- extensions/facebook/facebook.php
+++ extensions/facebook/facebook.php
@@ -10,6 +10,9 @@ class FacebookHandler extends Orbit{
 		parent::__construct();
 		
 		$this->facebook_active = $this->returnPref('facebook_active');
+		$this->facebook_name = $this->returnPref('facebook_name');
+		$this->facebook_album_id = $this->returnPref('facebook_album_id');
+		$this->facebook_access_token = $this->returnPref('facebook_access_token');
 		$this->facebook_format_image = $this->returnPref('facebook_format_image');
 		$this->facebook_last_image_time = $this->returnPref('facebook_last_image_time');
 		
@@ -18,7 +21,8 @@ class FacebookHandler extends Orbit{
 		$this->facebook_name = $this->returnPref('facebook_name');
 		
 		$config = array('appId' => '7c4c834772300f94f220af8c5198cd4a',
-			'secret' => '3623ff780be9e6ebcc31eed8f2100888');
+			'secret' => '3623ff780be9e6ebcc31eed8f2100888',
+			'fileUpload' => true);
 		
 		$this->facebook = new Facebook($config);
 	}
@@ -28,6 +32,15 @@ class FacebookHandler extends Orbit{
 	}
 	
 	public function orbit_config(){
+		$params = array('access_token' => $this->facebook_access_token);
+		$albums = $this->facebook->api('me/albums', $params);
+		
+		$albums_list = array();
+		
+		foreach($albums['data'] as $album){
+			$albums_list[$album['id']] = $album['name'];
+		}
+		
 		?>
 		<p>Every time you publish an image, it will be uploaded to <a href="http://www.facebook.com/">Facebook</a>.</p>
 		<?php
@@ -37,10 +50,20 @@ class FacebookHandler extends Orbit{
 			<table>
 				<tr>
 					<td class="right"><label>Name:</label></td>
-					<td><a href="http://<?php echo $this->facebook_name; ?>.facebook.com/"><?php echo $this->facebook_name; ?></a> &#0160; <button><a href="<?php echo $this->locationFull(array('unlink' => 'facebook')); ?>">Unlink from Facebook</button></a></td>
+					<td><?php echo $this->facebook_name; ?> &#0160; <a href="<?php echo $this->locationFull(array('unlink' => 'facebook')); ?>"><button>Unlink from Facebook</button></a></td>
 				</tr>
 				<tr>
-					<td class="right pad"><label for="facebook_format_image">Image format:</label></td>
+					<td class="right"><label for="facebook_album_id">Facebook album:</label></td>
+					<td>
+						<select id="facebook_album_id" name="facebook_album_id">
+							<?php foreach ($albums_list as $id => $name): ?>
+								<option value="<?php echo $id ?>" <?php if($id == $this->facebook_album_id){ echo 'selected="selected"'; } ?>><?php echo $name; ?></option>
+							<?php endforeach ?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td class="right"><label for="facebook_format_image">Image caption format:</label></td>
 					<td>
 						<textarea type="text" id="facebook_format_image" name="facebook_format_image" style="width: 30em;" class="code"><?php echo $this->facebook_format_image; ?></textarea><br />
 						<span class="quiet">Your image will automatically be posted, use the text area above to write an optional caption. Use Canvas tags such as <code>{Image_Title}</code> and <code>{Image_URI}</code> above.</span>
@@ -68,24 +91,29 @@ class FacebookHandler extends Orbit{
 		if(!empty($_GET['from'])){
 			switch($_GET['from']){
 				case 'facebook':
-					var_dump($_REQUEST);
 					/*
-					$tumblr_access_token = $this->tumblr->getAccessToken($_GET['oauth_verifier']);
+					$params = array('client_id' => '7c4c834772300f94f220af8c5198cd4a',
+						'redirect_uri' => 'http://www.alkalineapp.com/callback/',
+						'client_secret' => '3623ff780be9e6ebcc31eed8f2100888',
+						'code' => $_GET['code']);
 					
-					$this->tumblr_active = true;
-					$this->setPref('tumblr_active', true);
+					$here = $this->facebook->api('oauth/access_token', $params);
+					*/
 					
-					$user = $this->tumblr->post('authenticate');
-					$xml = simplexml_load_string($user);
-					$this->setPref('tumblr_name', (string)$xml->tumblelog['name']);
+					$access_token = $_REQUEST['access_token'];
 					
-					$this->setPref('tumblr_oauth_token', $tumblr_access_token['oauth_token']);
-					$this->setPref('tumblr_oauth_secret', $tumblr_access_token['oauth_token_secret']);
+					$params = array('access_token' => $access_token);
+					$me = $this->facebook->api('me', $params);
+					
+					$this->facebook_active = true;
+					$this->setPref('facebook_active', true);
+					$this->setPref('facebook_name', $me['name']);
+					$this->setPref('facebook_access_token', $access_token);
 					
 					$this->savePref();
 					
-					$this->addNote('You successfully linked your Tumblr account.', 'success');
-					*/
+					$this->addNote('You successfully linked your Facebook account.', 'success');
+					
 					header('Location: ' . $this->location());
 					exit();
 					
@@ -102,10 +130,15 @@ class FacebookHandler extends Orbit{
 					$this->setPref('facebook_oauth_token', $facebook_oauth_token);
 					$this->savePref();
 					
-					$params = array('next' => 'http://www.alkalineapp.com/',
-						'cancel' => 'http://www.alkalineapp.com/');
+					$params = array('next' => 'http://www.alkalineapp.com/callback/',
+						'cancel_url' => 'http://www.alkalineapp.com/callback/',
+						'req_perms' => 'user_photos, publish_stream, offline_access');
 					
-					header('Location: ' . $this->facebook->getLoginUrl($params));
+					header('Location: http://www.alkalineapp.com/callback/?' . http_build_query(
+						array('to' => $this->facebook->getLoginUrl($params),
+						'from' => $this->locationFull(array('from' => 'facebook'))))
+					);
+					
 					exit();
 			}
 		}
@@ -185,15 +218,28 @@ class FacebookHandler extends Orbit{
 			$image->hook();
 		}
 		
+		$this->setPref('facebook_album_id', @$_POST['facebook_album_id']);
+		$this->setPref('facebook_format_image', @$_POST['facebook_format_image']);
+		
 		$this->savePref();
 	}
 	
 	public function orbit_image($images){
 		if(count($images) < 1){ return; }
 		
-		// Seek array for new image
-		$latest = 0;
 		$now = time();
+		
+		if(!isset($image[0]['image_tags_array'])){
+			$image_ids = array();
+			
+			foreach($images as $image){
+				$image_ids[] = $image['image_id'];
+			}
+			
+			$images = new Image($image_ids);
+			$images->getTags();
+			$images = $images->images;
+		}
 		
 		foreach($images as $image){
 			$image_published = strtotime($image['image_published']);
@@ -203,57 +249,26 @@ class FacebookHandler extends Orbit{
 			if($image_published <= $this->facebook_last_image_time){ continue; }
 			if($image['image_privacy'] != 1){ continue; }
 			
-			if($image_published > $latest){
-				$latest = $image_published;
-				$latest_image = $image;
-			}
+			$this->storeTask(array($this, 'upload'), $image);
 		}
 		
-		if(empty($latest_image)){ return; }
-		if($latest_image['image_id'] == $this->facebook_last_image_id){ return; }
-		
-		// Get image, tags
-		$images = new Image($latest_image['image_id']);
-		$tags = $images->getTags();
-		$latest_image = $images->images[0];
-		
-		$latest_image_tags = array();
-		
-		foreach($tags as $tag){
-			$latest_image_tags[] = $tag['tag_name'];
-		}
-		
-		// Format tags
-		$latest_image_tags = '"' . implode('","', $latest_image_tags) . '"';
-		
-		// Save this image as last
-		$this->setPref('facebook_last_image_time', $latest);
-		$this->setPref('facebook_last_image_id', $latest_image['image_id']);
+		$this->setPref('facebook_last_image_time', $now);
 		$this->savePref();
-		
-		// Format caption
+	}
+	
+	public function upload($image){
 		$canvas = new Canvas($this->facebook_format_image);
-		$canvas->assignArray($latest_image);
+		$canvas->assignArray($image);
 		$canvas->generate();
 		
-		// Reformat relative links
-		$canvas->template = str_ireplace('href="/', 'href="' . LOCATION . '/', $canvas->template);
-		$canvas->template = str_ireplace('href=\'/', 'href=\'' . LOCATION . '/', $canvas->template);
-
-		$canvas->template = str_ireplace('src="/', 'src="' . LOCATION . '/', $canvas->template);
-		$canvas->template = str_ireplace('src=\'/', 'src=\'' . LOCATION . '/', $canvas->template);
+		$description = trim($canvas->template);
 		
-		$canvas->template = trim($canvas->template);
+		$file = file_get_contents($image['image_file']);
 		
-		// Send to Facebook
-		$parameters = array('type' => 'photo',
-			'format' => 'html',
-			'tags' => $latest_image_tags,
-			'source' => LOCATION . $latest_image['image_src'],
-			'caption' => $canvas->template,
-			'click-through-url' => $latest_image['image_uri']);
-		
-		// $this->facebook->post('write', $parameters);
+		$params = array('access_token' => $this->facebook_access_token,
+			'source' => '@' . $image['image_file'],
+			'message' => $description);
+		$photos = $this->facebook->api($this->facebook_album_id . '/photos', 'POST', $params);
 	}
 }
 
