e15d74f452d34842c68541fa4331e2b4a1163d17
Jacob Budin <me@jacobbudin.com>
2011-05-24 23:15:33 -0400

    v1.1 SQL additions

--- update/1000/pgsql.sql
+++ update/1000/pgsql.sql
@@ -3,4 +3,4 @@ ALTER TABLE "posts" ADD "post_category" TEXT;
 ALTER TABLE "pages" ADD "page_category" TEXT;
 ALTER TABLE "users" ADD "user_uri" TEXT;
 ALTER TABLE "tags" ADD "tag_parents" TEXT;
-ALTER TABLE "guest_inclusive" ADD "guest_inclusive" INTEGER;
+ALTER TABLE "guests" ADD "guest_inclusive" INTEGER;
