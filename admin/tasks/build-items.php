271d212afe6ba9240d4c94e5953536dc319c8b4e
Jacob Budin <me@jacobbudin.com>
2011-05-25 10:41:18 -0400

    Build Items Table acknowledges existing IDs

--- admin/tasks/build-items.php
+++ admin/tasks/build-items.php
@@ -20,7 +20,7 @@ $id = $alkaline->findID(@$_POST['image_id']);
 if(empty($id)){
 	$ids = array();
 	
-	$alkaline->exec('DELETE FROM items WHERE item_id IS NOT NULL');
+	// $alkaline->exec('DELETE FROM items WHERE item_id IS NOT NULL');
 	
 	foreach($alkaline->tables_index as $key => $value){
 		$ids[] = ++$key;
@@ -29,10 +29,24 @@ if(empty($id)){
 	echo json_encode($ids);
 }
 else{
-	$ids = new Find($alkaline->tables_index[--$id]);
+	$table = $alkaline->tables_index[--$id];
+	
+	$ids = new Find($table);
 	$ids->find();
 	
+	$query = $alkaline->prepare('SELECT item_table_id FROM items WHERE item_table = :item_table;');
+	$query->execute(array(':item_table' => $table));
+	$items = $query->fetchAll();
+	
+	$item_table_ids = array();
+	
+	foreach($items as $item){
+		$item_table_ids[] = $item['item_table_id'];
+	}
+	
 	foreach($ids->ids as $item_id){
+		if(in_array($item_id, $item_table_ids)){ continue; }
+		
 		$fields = array('item_table' => $alkaline->tables_index[$id],
 			'item_table_id' => $item_id);
 		$alkaline->addRow($fields, 'items');
